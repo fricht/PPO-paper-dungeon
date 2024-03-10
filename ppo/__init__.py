c1225edca8ba332cb551d2b501608f0fad9b96e0
Gedeon.D <gedeon@deveaux.fr>
2024-03-10 22:02:53 +0100

    not bad... (not tested)

--- ppo/__init__.py
+++ ppo/__init__.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
 import numpy as np
+import env
 
 
 class Model(tf.keras.Model):
-    def __init__(self, output_neurons, activation_func=tf.keras.activations.sigmoid):
+    def __init__(self, output_neurons, activation_func=tf.nn.sigmoid):
         super().__init__()
         self.dense1 = tf.keras.layers.Dense(128, activation=activation_func)
         self.dense2 = tf.keras.layers.Dense(128, activation=activation_func)
@@ -15,8 +16,54 @@ class Model(tf.keras.Model):
 
 
 class PPO:
-    def __init__(self):
-        pass
+    def __init__(self, actions_neurons, gamma=0.95):
+        self.gamma = gamma
+        self.actions_neurons = actions_neurons
+        self.actor = Model(actions_neurons)
+        self.critic = Model(1, activation_func=None)
+        self.actor.compile(loss=tf.keras.losses.MeanSquaredError, optimizer='adam')
+
+    def compute_value_function(self, rewards):
+        returns = []
+        discounted = 0
+        for r in reversed(rewards):
+            discounted = r + self.gamma * discounted
+            returns.insert(0, discounted)
+        return returns
+
+    def train(self, env, epochs=100, episodes=10, batch_size=32, sub_epochs=10, max_iters_episode=1000):
+        for i in range(epochs):
+            print(i / epochs)
+            # actor perform in env
+            total_states = []
+            total_actions = []
+            total_rewards = []
+            total_value = []
+            for _ in range(episodes):
+                states = [env.reset()]
+                actions = []
+                rewards = []
+                for j in range(max_iters_episode):
+                    action = None
+                    state, reward, done = env.step(action)
+                    actions.append(action)
+                    rewards.append(reward)
+                    if done:
+                        break
+                    states.append(state)
+                values = self.compute_value_function(rewards)
+                total_states += states
+                total_actions += actions
+                total_rewards += rewards
+                total_value += values
+            # train critic
+            self.critic.fit(x=np.array([total_states, total_actions]).transpose(), y=np.array([total_value]).transpose(), batch_size=batch_size, epochs=sub_epochs)
+            # eval actions weights
+            weighted_best = []
+            for state in states:
+                weighted_best.append()
+            weights = self.critic.predict(np.identity(self.state_size)).numpy().flatten()
+            #
 
 
 if __name__ == "__main__":
