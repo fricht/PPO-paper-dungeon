a634bd9d5bb77b6f9e808b4320fd23ccd1a56ecb
Gedeon.D <gedeon@deveaux.fr>
2024-03-12 15:21:22 +0100

    saving commit

---
+++ ppo/__init__.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+class NN:
+    def __init__(self, input_size, neurons, activation_function):
+        pass
---
+++ ppo/activations.py
@@ -0,0 +1,5 @@
+class Linear:
+    def func(n):
+        return n
+    def deriv(n):
+        return 1
